6b10d8799fe43c96994ba3b2178c23cf2702dfdd
Aizhan-15 <aizhanurman@gmail.com>
2022-09-20 19:33:42 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task2;
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
         int first1 = 12;
-        int second1 = 23;
+        int second1 = 12;
         if (first1 >= second1) {
             System.out.println(first1);
         } else {
