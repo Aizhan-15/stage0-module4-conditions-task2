0e4c5654818e8e92696669082c52957b49a28ef0
Aizhan-15 <aizhanurman@gmail.com>
2022-09-20 19:26:34 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        int number = 499;
+        if (number < 500) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int first1 = 32;
+        int second1 = 23;
+        if (first1 > second1) {
+            System.out.println(first1);
+        } else {
+            System.out.println(second1);
+        }
     }
 }
